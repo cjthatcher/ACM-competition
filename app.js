e58837b27c42ffe1a2de304d69b7edac50e6de02
Cade Ward <cadew@live.com>
2013-12-13 15:04:01 -0700

    added code for mongo db

--- app.js
+++ app.js
@@ -38,3 +38,39 @@ fs.readdirSync(__dirname + "/routes").forEach(function(file){
   require('./routes/' + file)(app);
 });
 
+
+// below is code for mongo db. I had to install mongodb first.
+// On mac, brew install mongodb. Christ - Good Luck. I'm sure there's a way. 
+// First - it creates a collection. Idk if this is really needed? - clarify if you know
+// Second - it adds some sample data to a var and inserts the record into the collection
+// Last - I included code to find the record you just inserted.
+
+// var MongoClient = require('mongodb').MongoClient
+//     , format = require('util').format;
+
+// MongoClient.connect("mongodb://localhost:27017/testCollection", function(err, db) {
+//   if(!err)
+//     console.log("We are connected");
+
+//   // //------------------------------create collection---
+// 	// db.createCollection("users", function(err, collection){
+// 	//    if (err) throw err;
+
+// 	//    	console.log("[INFO] Collection created.");
+// 	// }); // ---------------------------------------------
+
+// 	//json record
+// 	//var sampleData = {username:"cade", password:"secret"};
+  
+// 	//------------------------------------insert record----
+// 	// db.collection('testCollection').insert(sampleData, function(err, records) {
+// 	// 	if (err) throw err;
+// 	// 	console.log(records[0].username);
+// 	// }); // ---------------------------------------------
+
+// 	// --------------find the data previously entered -----
+// 	db.collection('testCollection').find({username: "cade"}).toArray(function(err, results){
+// 		console.log(results[0].username); // prints user name
+// 		console.log(results[0].password); // prints password
+// 	}); // ------------------------------------------------
+// });
