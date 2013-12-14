f6d09c9038f176d43fdfd9e7fd21b58c217f3f05
numso <dosmun@gmail.com>
2013-12-14 16:45:19 -0700

    angular is now setup. woot

---
+++ public/js/app/controller/header.js
@@ -0,0 +1,7 @@
+/* global angular */
+angular.module('acm').controller('headerCtrl',
+  function ($scope, nav) {
+    'use strict';
+    $scope.nav = nav;
+  }
+);
--- public/js/app/controller/signup.js
+++ public/js/app/controller/signup.js
@@ -1,12 +1,11 @@
 /* global angular */
-'use strict';
-
 angular.module('acm').controller('signupCtrl',
-  function ($scope, $http) {
+  function ($scope, $http, $state) {
+    'use strict';
 
-    $http.get("/user").success(function(data){
+    $http.get('/user').success(function (data) {
       if(data.success)
-        $scope.user = data.user;
+        $state.transitionTo('index');
     });
 
     $scope.signup = function() {
@@ -18,37 +17,16 @@ angular.module('acm').controller('signupCtrl',
           email: $scope.email
       };
 
-      $http.post('/signup', user).success(function(data) {
-        console.log('w00t!');
-        console.log(data);
+      $http.post('/signup', user).success(function (data) {
         if (!data.success)
           $scope.error = data.err;
-        else
+        else {
           $scope.user = data.user;
-      })
-
-      return false;
-    }
-
-    $scope.logout = function() {
-      $http.post('/logout').success(function(){
-        $scope.user = false;
-        window.location.reload();
-      })
-    }
-    $scope.login = function() {
-      var user = {
-        username: $scope.username,
-        password: $scope.password
-      }
-
-      $http.post('/login', user).success(function(data){
-        if (!data.success)
-          $scope.error = data.err;
-        else
-          $scope.user = data.user;
+          $state.transitionTo('index');
+        }
       });
+
       return false;
-    }
+    };
   }
 );
